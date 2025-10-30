61eb2e2da89010c2a3ada16dd537d8fb9ac6a955
Nicolas <2120666@stud.hs-mannheim.de>
2025-10-30 20:40:34 +0100

    GPIO Pin Konfiguration

--- RT_Application_Drivers/RTCore_Blank/main.c
+++ RT_Application_Drivers/RTCore_Blank/main.c
@@ -6,19 +6,72 @@
 #include "lib/UART.h"
 #include "lib/Print.h"
 #include "lib/CPUFreq.h"
+#include"lib/GPIO.h"
 #include "nvic.h"
 #include"timers.h"
+#include"timer.h"
+#include"Common.h"
 
 
 
 static UART *debug = NULL;
 static UART *driver = NULL;
+bool *state = false;
 
 
 void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName)
 {
     for(;;);
 }
+
+
+static void GPIO_Init(){
+
+    GPIO_ConfigurePinForOutput(0);
+    GPIO_ConfigurePinForOutput(8);
+    GPIO_ConfigurePinForOutput(15);
+    GPIO_ConfigurePinForOutput(18);
+    GPIO_ConfigurePinForOutput(21);
+    
+    GPIO_Write(0,false);
+    GPIO_Write(8,false);
+    GPIO_Write(15,false);
+    GPIO_Write(18,false);
+    GPIO_Write(21,false);
+
+
+
+}
+
+static void measure(){
+
+    GPIO_Write(0,true);
+    vTaskDelay(pdMS_TO_TICKS(0.01));
+    GPIO_Write(0,false);
+    unsigned long long timestamp_1 = get_current_system_us();
+
+
+    int ret = GPIO_ConfigurePinForInput(0);
+    if(ret != ERROR_NONE){
+        UART_Print(debug,"Fehler beim umschalten"); 
+    }
+
+    while(*state == false){
+        GPIO_Read(0,state);
+    }
+    unsigned long long timestamp_2 = get_current_system_us();
+
+    unsigned long long pulse = timestamp_2 - timestamp_1;
+
+    float distance = pulse / 58.0f;
+
+
+    UART_Print(debug,"%f");
+
+
+
+}
+
 
 
 static void gpio_task(void *pParameters)
@@ -48,6 +101,8 @@ _Noreturn void RTCoreMain(void){
     UART_Print(debug, "--------------------------------\r\n");
     UART_Print(debug, "Hallo\r\n");
     UART_Print(debug, "App built on: " __DATE__ " " __TIME__ "\r\n");
+
+    GPIO_Init();
 
 
     xTaskCreate(gpio_task, "BLINKI", 256, NULL, 5, NULL);
